918fc5eeb3562674a423a4a530cb16db43bdbb7e
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-03 08:51:28 -0700

    Create 113.rb

---
+++ 113.rb
@@ -0,0 +1,19 @@
+show = 30
+steven = 20
+iphone = 15
+
+sum = show + steven + iphone
+p "The sum of the three numbers is: #{sum}"
+
+# QUESTION 2
+# If you run the code below, it will crash due to invalid variable names. 
+# Find the invalid variable names and delete the special characters 
+# (be sure to leave the special characters in the valid variable names alone).
+bob7 = 50
+# 7bob = 50
+# bob@ = 50
+# @bob = 50
+bob_ = 50
+ _bob = 50
+# bob$ = 50
+ $bob = 50
