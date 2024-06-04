5123551648b5e963dac414ea60938b4b18c3bb6d
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-04 09:59:00 -0700

    Update 134.rb

--- 134.rb
+++ 134.rb
@@ -1,2 +1,29 @@
+# QUESTION 1
+# Write code using a while loop that asks the user to enter a number 
+# and will run forever until the user enters a number greater than 10.
+
+number = 0
+
+while number <= 10
+  puts "Enter a number:"
+  number = gets.chomp.to_i
+end
+
+p "You entered a number greater than 10!"
 
 
+# QUESTION 2
+# The following code will run 100 times, and each time it will ask the user for their name. 
+# The code will break early if the user's name is Bob.
+# Rewrite the code using a while loop so the program will run *forever* unless 
+# the user enters a name of Bob.
+
+
+while true
+    puts "What is your name?"
+    name = gets.chomp
+    if name == "Bob"
+      puts "Hi, Bob!"
+      break
+    end
+  end
