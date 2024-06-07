cae8f7161e0c2c3551a418bac1a8ad6a21835f3c
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-07 11:08:20 -0700

    Create 232.rb

---
+++ 232.rb
@@ -0,0 +1,17 @@
+# QUESTION 1
+# Write code that prints all the years between 1980 to 2020 (including 1980 and 2020).
+
+(1980..2020).each do |year|
+    puts year
+  end
+  
+
+  # QUESTION 2
+# The following code is supposed to print the numbers 1 through 10, but it has errors.
+# Fix the errors with the code below.
+
+count = 1
+while count <= 10
+  puts count
+  count += 1
+end
