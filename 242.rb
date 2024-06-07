0b257feaf3529d476d1c059d35ce990b7f4f2b6d
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-07 12:32:53 -0700

    Create 242.rb

---
+++ 242.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+chips = ["potato", "corn", "tortilla"]  
+chips.push("pita")                      
+pp chips                   
+
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+
+
+chips = {
+  "potato" => "A thin slice of potato that has been deep fried or baked until crunchy.",
+  "corn" => "A crunchy chip made from cornmeal, often enjoyed with salsa."
+}
+chips["tortilla"] = "A chip made from corn or wheat tortilla, usually triangular and often served with dips."
+pp chips 
