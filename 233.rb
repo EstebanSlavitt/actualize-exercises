88fc258461698e6fa09523038c9b83e89dbdb463
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-07 11:33:58 -0700

    Update 233.rb

--- 233.rb
+++ 233.rb
@@ -37,3 +37,25 @@ color.each do |colors|
   puts color.upcase
   index = index + 1
 end
+
+
+
+
+
+
+
+# QUESTION 2
+# The following code is supposed to print "OUTER LOOP" followed by "inner loop" three times,
+# then print "OUTER LOOP" followed by "inner loop" three times again.
+# However, there is a mistake in the code that causes the code to run forever.
+# Find and fix the mistake.
+index = 0
+while index < 2
+  puts "OUTER LOOP"
+  index2 = 0
+  while index2 < 3
+    puts "inner loop"
+    index2 = index2 + 1  
+  end
+  index = index + 1  
+end
