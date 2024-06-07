49ac34cb759d3ab2e1a09188ffa6f7f0cbe05ef4
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-07 09:21:10 -0700

    Create 212.rb

---
+++ 212.rb
@@ -0,0 +1,26 @@
+# QUESTION 1
+# Write code to store a number using a variable, then a string using a different variable.
+# Then add the two variables together (by converting the number to a string) and 
+# store the result in a third variable. Finally, print out the third variable.
+
+
+
+
+# number = 42
+
+# text = " It's really pretty outside"
+# result = number.to_s + text
+# puts result
+
+# got a little confused is this the answer i'm looking for "42 It's really pretty outside"
+
+# QUESTION 2
+# The code below has a mistake in it. Run the code to see the error message,
+# then explain what the error message means and what would need to change to fix it.
+# x = 10
+# puts x + y  # y is not defined so we need to define it before we print it. 
+# y = x
+
+x = 10
+y = x
+puts x + y
