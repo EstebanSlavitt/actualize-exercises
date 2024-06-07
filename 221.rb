32e504feeeefb885d898df06a624c3988680a27d
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-07 10:10:30 -0700

    Create 221.rb

---
+++ 221.rb
@@ -0,0 +1,12 @@
+# DELIBERATE PRACTICE REFLECTION
+
+# Write a while loop that prints the phrase "Around the world" 144 times.
+
+number = 0
+while number < 144
+  puts "Around the world"
+  number = number + 1
+end
+
+
+# i was able todo this loop with out looking, next time ill find a harder loop. 
