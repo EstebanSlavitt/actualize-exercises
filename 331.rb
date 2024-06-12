51ddc07bf04d1dd0ff6d819691599f7852a9148a
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-12 12:36:17 -0700

    Create 331.rb

---
+++ 331.rb
@@ -0,0 +1,9 @@
+# Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+puts "What is your name?"
+name = gets.chomp
+if name != "Santa"
+  puts "You're not Santa."
+end
+
+# i had to google != which now i know means n0t equal. i guess i forgot 
