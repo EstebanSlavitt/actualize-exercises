75b89acdd10eba5aaee69313fe6252dba893026c
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-12 11:08:50 -0700

    Create 314.rb

---
+++ 314.rb
@@ -0,0 +1,25 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Ancay ouyay igurefay histay neoay utoay?
+# Pig Latin is a very sophisticated language in which 
+# each English word is converted as follows: 
+# The first letter of each word is removed from the beginning and added to the end. 
+# Then, the letters 'ay' are added to the very end of the word.
+# For example: hello = ellohay, pingpong = ingpongpay, marmalade = armalademay, etc.
+# Write a program that asks the user to enter a single word 
+# and prints out the Pig Latin version of that word.
+
+# Add a single word
+puts "Enter a single word:"
+word = gets.chomp
+
+def pig_latin(word)
+    first_letter = word[0]
+    rest_of_word = word[1..-1]
+    pig_latin_word = rest_of_word + first_letter + "ay"
+    pig_latin_word
+  end
+  
