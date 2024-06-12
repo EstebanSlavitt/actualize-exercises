1b62fcffe0c57b51f333b6c3228039d235e8be87
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-12 12:05:29 -0700

    Update 321.rb

--- 321.rb
+++ 321.rb
@@ -7,3 +7,18 @@ if word == "marco"
 end
 
 # i had to look at it i i just put = instead of ==
+
+# QUESTION 1
+# Use a loop to create a new array of strings from each hash's :name key.
+dogs = [
+  { name: "Toto", breed: "Cairn Terrier" },
+  { name: "Snoopy", breed: "Beagle" },
+  { name: "Beethoven", breed: "Saint Bernard" }
+]
+
+dog_names = []
+dogs.each do |dog|
+
+  dog_names << dog[:name]
+end
+puts dog_names
