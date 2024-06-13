e8eab04cca9322b0e9664028147f113991d8535c
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-13 10:22:53 -0700

    Create 341.rb

---
+++ 341.rb
@@ -0,0 +1,8 @@
+# Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator)
+
+book_title = "Harry Potter and the Philosopher's Stone "
+book_author = "J. K. Rowling"
+puts "The author of #{book_title} is #{book_author}."
+
+
+# i was able todo this first try. 
