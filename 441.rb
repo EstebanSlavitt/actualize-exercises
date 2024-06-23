04b4e8a47960953d8be266ca828dea370f27efa6
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-23 09:34:48 -0700

    Create 441.rb

---
+++ 441.rb
@@ -0,0 +1,10 @@
+# Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+
+countries = ["USA", "Canada", "Mexico"]
+countries << "Japan"
+
+
+puts countries.join(", ")
+
+# i forgot to put (", ")
