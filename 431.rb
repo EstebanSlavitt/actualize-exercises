9750c7a0966159cc40f326964f07316895e8f102
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-23 09:04:15 -0700

    Create 431.rb

---
+++ 431.rb
@@ -0,0 +1,13 @@
+# Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+
+names = ["steven", "esteban", "slavitt"]
+
+index = 0
+while index < names.length
+  puts names[index]
+  index += 1
+end
+
+
+# i got this first try 
