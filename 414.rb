65a53b4807145b52648cd90ecd0c9d355dca416c
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-23 08:09:04 -0700

    414.rb

---
+++ 414.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Read about the Ruby File class: https://rubyapi.org/3.2/o/s?q=file
+# Then write a program that creates a file called example.txt with the 
+# sentence "This is an example!" in it.
+
+
+
+File.open("example.txt", "w") do |file|
+    file.write("This is an example!")
+  end
+  
+  
