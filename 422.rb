88f9e33ee213632ebef2e89b6d06bc1fb17908dc
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-23 08:21:34 -0700

    Create 422.rb

---
+++ 422.rb
@@ -0,0 +1,12 @@
+# QUESTION 1
+# Write a method that takes in a string and a number n and returns the string repeated n times.
+# Then run the method and print the result.
+
+
+def repeat_string(str, n)
+    str * n
+  end
+  
+  result = repeat_string("You did it! yay! ", 3)
+  puts result
+  
