d5b98423efcc9d96e2d39ab4c5c3242457750c32
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-27 11:13:01 -0700

    Create 512.rb

---
+++ 512.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+# Write a method that takes in the person hash below and increases the age by 1.
+# Then run the method and print the person hash to see if the hash was changed.
+# person = { name: "Shawn", age: 25 }
+
+
+
+
+
+
+def increment_age(person)
+    person[:age] += 1
+  end
+  
+  person = { name: "Shawn", age: 25 }
+  
+  increment_age(person)
+  
+  
+  puts person
+  
