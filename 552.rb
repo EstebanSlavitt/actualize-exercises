65d12f6c7baad3ab1cc7ebcd27805655bd6327ad
esteban slavitt <152658032+EstebanSlavitt@users.noreply.github.com>
2024-06-29 12:05:40 -0700

    Create 552.rb

---
+++ 552.rb
@@ -0,0 +1,15 @@
+# QUESTION 1
+# Write a method that takes in three strings and uses string interpolation 
+# to return all three strings combined with spaces in between as a single string. 
+# Then run the method and print the result.
+
+
+
+def combine_strings(str1, str2, str3)
+    "#{str1} #{str2} #{str3}"
+  end
+  
+  result = combine_strings("Hello", "Master", "im using ruby")
+  puts result
+  
+  # this was kinda hard to figure out 
